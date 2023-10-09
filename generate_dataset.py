7615bf850a28f05248e876852bb5a5728735e4e3
Federico Lancellotti <federicolancellotti@WheezyTePenguin.lan>
2023-10-09 22:07:36 +0200

    First commit

---
+++ generate_dataset.py
@@ -0,0 +1,12 @@
+import gym
+import numpy as np
+import os
+import torch
+import argparse
+
+from logger import Logger
+from utils import stack_frames
+
+# from Envs.DoublePendulum import AcrobotEnv
+
+test = 0
