303bf211d1825d314253491f1e215e09c6fe9a13
Federico Lancellotti <fedelancell@gmail.com>
2023-10-19 23:34:14 +0200

    bug fix

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import pickle
 import cv2
 
 
+# Saves and loads the data as a pickle file
 def save_pickle(filename, data):
     with open(filename, 'wb') as f:
         pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
@@ -17,6 +18,7 @@ def load_pickle(file):
     return data
 
 
+# Concatenates two consecutive frames along the channel dimension
 def stack_frames(prev_frame, frame, size1=84, size2=84):
     prev_frame = cv2.resize(prev_frame, (size1, size2))
     frame = cv2.resize(frame, (size1, size2))
@@ -24,6 +26,8 @@ def stack_frames(prev_frame, frame, size1=84, size2=84):
     return stacked_frames
 
 
+# Returns a np.array random batch of dimension batch_size
 def sample_batch(data, batch_size=32):
     idxs = np.random.randint(0, len(data), batch_size)
-    return data[idxs]
+    batch = [data[i] for i in idxs]
+    return np.array(batch)
